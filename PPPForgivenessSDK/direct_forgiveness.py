6c275c570890256c0672b7af7364745f3bcb33bc
landerson7823 <landerson7823@gmail.com>
2021-06-28 17:28:47 -0500

    Add some API functionality for Direct Forgiveness & files for pypi packaging

---
+++ PPPForgivenessSDK/direct_forgiveness.py
@@ -0,0 +1,114 @@
+import json
+from .base_api import BaseApi
+
+
+
+class DirectForgivenessApi(BaseApi):
+
+    def status(self, slug):
+        http_method = "GET"
+        endpoint = "direct_forgiveness_status/{0}/".format(slug)
+
+        uri = self.client.api_uri + endpoint
+
+        response = self.execute(http_method=http_method,
+                                url=uri)
+
+        return {'status': response.status_code,
+                'data': json.loads(response.text)}
+
+    def eligibility(self, sbanumber, tin):
+        http_method = "GET"
+        endpoint = "direct_forgiveness_eligibility/?sba_number={0}&tin={1}".format(sbanumber, tin)
+
+        uri = self.client.api_uri + endpoint
+
+        response = self.execute(http_method=http_method,
+                                url=uri)
+
+        return {'status': response.status_code,
+                'data': json.loads(response.text)}
+
+    def create(self,
+               entity_name,
+               address1,
+               address2,
+               ein,
+               naics_code,
+               primary_name,
+               primary_email,
+               phone_number,
+               ppp_loan_draw,
+               sba_number,
+               loan_number,
+               bank_notional_amount,
+               funding_date,
+               forgive_covered_period_from,
+               forgive_covered_period_to,
+               forgive_fte_at_loan_application,
+               forgive_fte_at_forgiveness_application,
+               forgive_2_million,
+               forgive_payroll,
+               forgive_amount,
+               dba_name=None,
+               demographics=[],
+               loan_increase=None,
+               loan_increase_date=None):
+        http_method = "POST"
+        endpoint = "direct_forgiveness_submit/"
+        uri = self.client.api_uri + endpoint
+
+        params = {
+            "entity_name": entity_name,
+            "address1": address1,
+            "address2": address2,
+            "ein": ein,
+            "naics_code": naics_code,
+            "primary_name": primary_name,
+            "primary_email": primary_email,
+            "phone_number": phone_number,
+            "ppp_loan_draw": ppp_loan_draw,
+            "sba_number": sba_number,
+            "loan_number": loan_number,
+            "bank_notional_amount": bank_notional_amount,
+            "funding_date": funding_date,
+            "forgive_covered_period_from": forgive_covered_period_from,
+            "forgive_covered_period_to": forgive_covered_period_to,
+            "forgive_fte_at_loan_application": forgive_fte_at_loan_application,
+            "forgive_fte_at_forgiveness_application": forgive_fte_at_forgiveness_application,
+            "forgive_2_million": forgive_2_million,
+            "forgive_payroll": forgive_payroll,
+            "forgive_amount": forgive_amount,
+        }
+
+        # optional fields
+        if dba_name: params['dba_name'] = dba_name
+        if demographics: params['demographics'] = demographics
+        if loan_increase: params['loan_increase'] = loan_increase
+        if loan_increase_date: params['loan_increase_date'] = loan_increase_date
+
+        headers = {'Content-Type': 'application/json'}
+        response = self.execute(http_method=http_method,
+                                headers=headers,
+                                url=uri,
+                                data=json.dumps(params))
+
+        return {'status': response.status_code,
+                'data': json.loads(response.text)}
+
+    def delete(self, *args, **kwargs):
+        # Will implement once endpoint made in SBA
+        pass
+
+    def create_document(self, name, etran_loan, document_type, document):
+        http_method = "POST"
+        endpoint = "direct_forgiveness_document/"
+        uri = self.client.api_uri + endpoint
+        params = {'name': name, 'document_type': document_type, 'etran_loan': etran_loan}
+        files = {'document': open(document, 'rb')}
+        response = self.execute(http_method=http_method,
+                                url=uri,
+                                data=params,
+                                files=files)
+        return {'status': response.status_code,
+                'data': json.loads(response.text)}
