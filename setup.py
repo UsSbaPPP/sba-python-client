fb815a7273e6cd3794a914ed5e33a208920d8d9a
landerson7823 <landerson7823@gmail.com>
2021-07-01 16:10:53 -0500

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='PPPForgivenessSDK',
-   version='1.0',
+   version='1.1',
    description='api support for Paycheck Protection Program Portal',
    author='',
    author_email='',
